80acbb55a6a7ab89eb36bac7204b4cd5ad7dd71e
Aleksandar Angelov <aleksandarangelov.edu@gmail.com>
2026-06-17 23:15:45 -0400

    Add Authorization header to Edge Function call

--- script.js
+++ script.js
@@ -373,7 +373,6 @@ function closeInlineForm() {
   selectedRating = 0;
   selectedYear = '';
   // hcaptcha may not be loaded yet if the script is still fetching.
-  // hcaptcha may not be loaded yet if the script is still fetching.
   if (typeof hcaptcha !== 'undefined') hcaptcha.reset();
 }
 
@@ -441,7 +440,10 @@ async function submitReview() {
   try {
     res = await fetch('https://qqbfiwixlqsnjsmwirtf.supabase.co/functions/v1/submit-review', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxYmZpd2l4bHFzbmpzbXdpcnRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5Mjc2OTEsImV4cCI6MjA5MTUwMzY5MX0.Qy2_QBt4l2uRPiLQIKAaao4gNwZf0bkniUob9EtBXMY',
+      },
       body: JSON.stringify({
         dormId: currentDorm.id,
         name,
